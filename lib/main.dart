3e1040aee50de031e6fd73473241c8a212c7ce3a
Syed Waleed Nawaz <syedwaleednawaz333@gmail.com>
2023-01-31 10:10:32 +0500

    add overall concept

--- lib/main.dart
+++ lib/main.dart
@@ -362,6 +362,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:push_notification/view/localnotificationservice/localnotificationservice.dart';
 import 'package:push_notification/view/login.dart';
 import 'package:push_notification/view/map/places.dart';
@@ -372,7 +373,6 @@ import 'view/map/address_match.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   LocalNotificationService.initialize();
   runApp(MyApp());
@@ -484,10 +484,10 @@ Future<void> backgroundHandler(RemoteMessage message) async {
 
 // Future<dynamic> myBackgroundMessageHandler(RemoteMessage message) async {
 //   print("onBackground: ${message.notification!.title}/${message.notification!.body}/${message.notification!.titleLocKey}");
-//   // var androidInitialize = new AndroidInitializationSettings('notification_icon');
-//   // var iOSInitialize = new IOSInitializationSettings();
-//   // var initializationsSettings = new InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
-//   // FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-//   // flutterLocalNotificationsPlugin.initialize(initializationsSettings);
-//   // NotificationHelper.showNotification(message, flutterLocalNotificationsPlugin, true);
+//   var androidInitialize = new AndroidInitializationSettings('notification_icon');
+//   var iOSInitialize = new IOSInitializationSettings();
+//   var initializationsSettings = new InitializationSettings(android: androidInitialize, iOS: iOSInitialize);
+//   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//   flutterLocalNotificationsPlugin.initialize(initializationsSettings);
+//   NotificationHelper.showNotification(message, flutterLocalNotificationsPlugin, true);
 // }
